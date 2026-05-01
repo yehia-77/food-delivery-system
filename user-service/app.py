92d34f7daeec0afe75c4da6054115d124fa6e40a
yehia-77 <yehia.talaat.omar@gmail.com>
2026-05-01 09:33:34 -0400

    Fix user service indentation

--- user-service/app.py
+++ user-service/app.py
@@ -6,26 +6,25 @@ users = []
 
 @app.route('/health', methods=['GET'])
 def health():
-  return jsonify({"status": "ok", "service": "user-service"})
+    return jsonify({"status": "ok", "service": "user-service"})
 
 @app.route('/register', methods=['POST'])
 def register():
-  data = request.json
-  if not data or 'name' not in data or 'email' not in data:
-     return jsonify({"error": "name and email required"}), 400
-  user = {
-     "id":len(users) + 1,
-     "name": data['name'],
-     "email": data['email'],
-     "role": data.get('role', 'customer')
-   }
-   users.append(user)
-   return jsonify({"message":"user registered!", "user":user}),201
+    data = request.json
+    if not data or 'name' not in data or 'email' not in data:
+        return jsonify({"error": "name and email required"}), 400
+    user = {
+        "id": len(users) + 1,
+        "name": data['name'],
+        "email": data['email'],
+        "role": data.get('role', 'customer')
+    }
+    users.append(user)
+    return jsonify({"message": "User registered!", "user": user}), 201
 
 @app.route('/users', methods=['GET'])
 def get_users():
-  return jsonify(users)
+    return jsonify(users)
 
-
-if __name__=='__main__':
-  app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
